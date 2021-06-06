a2a0eea337b53ad1d7c9f110252e12473c062558
Ryo Ando <sasigume@gmail.com>
2021-06-06 17:43:09 +0900

    [css] polished design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,20 +3,21 @@ import { render } from '@9gustin/react-notion-render';
 import { LayoutProps } from 'models';
 
 const Layout = ({ notionPage }: LayoutProps) => (
-  <div className="container mx-auto max-w-xl flex flex-col gap-4">
+  <div className="container mx-auto max-w-xl flex flex-col gap-4 py-8 px-3">
     <Head>
       <title>{(notionPage && notionPage.title) ?? 'No title'}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <main area-label="Main content" className="px-3">
+    <main area-label="Main content">
       <article>
-        <h1 className="text-4xl font-bold mt-12">
+        <h1 className="text-4xl font-bold mt-6">
           {(notionPage && notionPage.title) ?? 'No title'}
         </h1>
         {notionPage && render(notionPage.blocks.results, true)}
       </article>
     </main>
+    <hr className="my-8" />
     <footer area-label="Footer">
       <span>Made with Notion</span>
     </footer>
--- src/models/notion/page.d.ts
+++ src/models/notion/page.d.ts
@@ -1,36 +1,14 @@
 // https://api.notion.com/v1/pages/page_id
 
 export interface Page {
-  object: string;
   id: string;
-  created_time: string;
-  last_edited_time: string;
-  parent: {
-    type: string;
-    page_id: string;
-  };
-  archived: boolean;
   properties: {
     title: {
       id: string;
       type: string;
       title: [
         {
-          type: string;
-          text: {
-            content: string;
-            link: any;
-          };
-          annotations: {
-            bold: boolean;
-            italic: boolean;
-            strikethrough: boolean;
-            underline: boolean;
-            code: boolean;
-            color: string;
-          };
           plain_text: string;
-          href: any;
         }
       ];
     };
